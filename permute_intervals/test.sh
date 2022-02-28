127864a4df6621ec41f565504598f6685eda62d8
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2022-02-28 14:18:19 -0700

    refactored tests

--- permute_intervals/test.sh
+++ permute_intervals/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-go build intersect.go
+go build
 
-./intersect -g g.bed -b <(ls b*.bed | grep 'b[1-3]') -i 5000 -r 0
-./intersect -g g.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0
-./intersect -g g2.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0
-./intersect -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 50000 -r 0
+./permute_intervals -g g.bed -b <(ls b*.bed | grep 'b[1-3]') -i 5000 -r 0
+./permute_intervals -g g.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0
+./permute_intervals -g g2.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0
+./permute_intervals -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 50000 -r 0
--- permute_intervals/time.sh
+++ permute_intervals/time.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-go build intersect.go
+go build
 
-time ( ./intersect -g g.bed -b <(ls b*.bed | grep 'b[1-3]') -i 5000 -r 0 >/dev/null)
-time ( ./intersect -g g.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0 >/dev/null)
-time ( ./intersect -g g2.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0 >/dev/null)
-time ( ./intersect -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 50000 -r 0 >/dev/null)
-time ( ./intersect -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 500000 -r 0 >/dev/null)
-time ( ./intersect -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 5000000 -r 0 >/dev/null)
-time ( ./intersect -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 50000000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g.bed -b <(ls b*.bed | grep 'b[1-3]') -i 5000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g2.bed -b <(ls b*.bed | grep 'b[14]') -i 5000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 50000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 500000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 5000000 -r 0 >/dev/null)
+time ( ./permute_intervals -g g3.bed -b <(ls b*.bed | grep 'b[14]') -i 50000000 -r 0 >/dev/null)
