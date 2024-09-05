4b4fd3d3e2d82fa149e65e546a3b628617d5f8d1
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-09-05 16:28:26 -0600

    updated cmd/permute_intervals.go to just call a function

--- cmd/permute_intervals.go
+++ cmd/permute_intervals.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"os"
 	"github.com/jgbaldwinbrown/permuvals/pkg"
 )
 
 func main() {
-	flags := permuvals.GetFlags()
-	w := bufio.NewWriter(os.Stdout)
-	defer w.Flush()
-
-	comp, err := permuvals.FullCompare(flags)
-	if err != nil { panic(err) }
-	permuvals.FprintOvlsBed(w, comp.Overlaps)
-	if flags.Iterations > 0 {
-		permuvals.FprintProbs(w, comp.Probs)
-	}
+	permuvals.Full()
 }
